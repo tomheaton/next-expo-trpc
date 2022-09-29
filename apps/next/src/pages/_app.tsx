459717665ffd61ca9741ffd322667259066fe902
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-09-29 14:38:38 +0100

    update next version

--- apps/next/src/pages/_app.tsx
+++ apps/next/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import '@styles/globals.css'
-import type {AppType} from 'next/dist/shared/lib/utils';
+import type {AppType} from 'next/app';
 import {trpc} from "@utils/trpc";
 
 const App: AppType = ({Component, pageProps}) => {
